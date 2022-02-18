9aeb5e59ee91b36c2abd0e0fbc67c0523f69029a
ddangju <duswn4338@gmail.com>
2022-02-18 23:50:29 +0900

    ts 변수타입

---
+++ index.js
@@ -0,0 +1,15 @@
+//1. 변수에 타입을 지정해보자
+// 타입으로 string, number, boolean, null, undefined, bigint, [], {} 등
+
+////string
+var 이름 = "kim";
+
+////  스트링이 들어오는 어레이
+var 어레이 = ["kim", "park"];
+
+////object자료형
+var 오브젝트 = { name: "kim" };
+
+
+/// name이라는 속성이 들어올 수도 있고 안들어올수도있따
+var 오브젝트 = { name?: "kim" };
---
+++ index.ts
@@ -0,0 +1,12 @@
+//1. 변수에 타입을 지정해보자
+// 타입으로 string, number, boolean, null, undefined, bigint, [], {} 등
+
+////string
+let 이름: string = "kim";
+
+////  스트링이 들어오는 어레이
+let 어레이: string[] = ["kim", "park"];
+
+////object자료
+
+let 오브젝트: { name: string } = { name: "kim" };
